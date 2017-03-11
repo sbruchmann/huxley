4c9431341a61994a7d5b61f909a7f7e75ade4135
Steffen Bruchmann <hi@sbruchmann.me>
2017-03-12 00:13:38 +0100

    Initial checkin

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+const electron = require("electron");
+
+const {app} = electron;
+
+let mainWindow;
+
+const createMainWindow = () => {
+	mainWindow = new electron.BrowserWindow({
+		title: app.getName()
+	});
+	mainWindow.loadURL("file://" + __dirname + "/index.html");
+	mainWindow.once("closed", () => mainWindow = null);
+};
+
+app.on("activate", () => {
+	if (!mainWindow) {
+		createMainWindow();
+	}
+});
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
+
+app.once("ready", createMainWindow);
